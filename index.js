103feccc751a58ce0915c07d5f25c640545715d3
Jarom Roney <byutrueblue@gmail.com>
2018-02-18 03:49:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ function takeANumber(katzDeliLine,name){
   katzDeliLine.push(name)
   return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name)+1} in line.`
 }
+function nowServing(){
+  if (katzDeliLine==0){
+    return 'There is nobody waiting to be served!'
+  }
+  else {
+    var person = katzDeliLine[0]
+    katzDeliLine.shift()
+    return `Currently serving ${person}`
+  }
+}
