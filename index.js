b050312ba0af5d1c656c64a1cf2251f4d014f1a6
Jarom Roney <byutrueblue@gmail.com>
2018-02-18 03:57:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,14 @@ function nowServing(katzDeliLine){
 }
 function currentLine(katzDeliLine){
   var output = 'The line is currently: '
-  for (var i = 0; i < katzDeliLine.length;i++){
-    output = output + (i+1) + '. ' + katzDeliLine[i] + ', '
+  if (katzDeliLine.length===0){
+    return 'The line is currently empty.'
+  }
+  else{
+    
+  
+    for (var i = 0; i < katzDeliLine.length;i++){
+      output = output + (i+1) + '. ' + katzDeliLine[i] + ', '
+    }
   }
 }
